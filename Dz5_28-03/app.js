7cb062b2355209b22e1663021896f776e7f1c2b9
Омурбек <pokomolodoy312@mail.com>
2023-07-25 20:57:06 +0600

    first connit

---
+++ Dz5_28-03/app.js
@@ -0,0 +1,13 @@
+let numbers = [3, 4, 5, 44, 55, 23, 34, 12, 26, 67, 77, 75, 98, 67]
+let newArray = numbers.filter((i) => i % 2 === 0)
+console.log(newArray);
+
+
+
+function names(...books){
+    let name = books.map((i) => i.books === 'у')
+    console.log(name);
+}
+names('dsadasd',)
+
+
